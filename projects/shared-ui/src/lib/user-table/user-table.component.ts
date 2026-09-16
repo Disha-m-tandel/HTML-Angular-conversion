536572742b2c5ca1b63e98cbf575db0a2a7e58d1
DISHA MADEV TANDEL <FZ00801@ims.mginets.com>
2026-09-16 09:22:01 +0530

    small changes

--- projects/shared-ui/src/lib/user-table/user-table.component.ts
+++ projects/shared-ui/src/lib/user-table/user-table.component.ts
@@ -7,12 +7,25 @@ import { TooltipDirective } from '../../../../../src/app/partials/directives/too
 
 
 @Component({
-    selector: 'app-user-table',
-    imports: [CommonModule, RouterModule, CapitalizeNamePipe, RowHighlightDirective, TooltipDirective],
-    templateUrl: './user-table.component.html',
-    styleUrl: './user-table.component.scss'
+  selector: 'app-user-table',
+  standalone: true,
+  imports: [CommonModule, RouterModule, CapitalizeNamePipe, RowHighlightDirective, TooltipDirective],
+  templateUrl: './user-table.component.html',
+  styleUrl: './user-table.component.scss'
 })
 export class UserTableComponent {
-@Input() users: any[] = []; //"I am a reusable component. Give me users from outside."
+@Input() users: any[] = []; //"I am a reusable component. Give me users from outside." And initially it will be empty array then when the parent data gives then to child then the data wil get store
 //here will get the users details from the parent component
+//@Input() users : receives/stores that data in the child component.
+
+//Note : Just don't think of [users] as a variable that stores the data. It's the binding that connects the two users properties.
+
+//@Input() users: any[] = []; means
+//           ↑     ↑       ↑
+//         name   type    value
+//users : This is the property/variable name. You're creating a property called users.
+//users : any[] : users is an array, and its items can be any type(number or boolean or string).
+//= [] : This gives users its initial/default value. [] means an empty array. So when the component first starts: users: any[] = []; empty array then Later, when the parent sends the actual data through @Input(): []
+                                                                                                                                                                                                            // ↓                    
+                                                                                                                                                                                              //[Sarah, Rafi, Nadia, Mina, Jon]
 }
